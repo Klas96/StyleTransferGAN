work on bugs current wrong dimension into block1_pool expected 4 got 5

StyleTransfer.py
@@ -23,16 +23,19 @@ content_layers = ['block4_conv2']
 style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
 
 # Load the content and style images
-content_image = np.array(PIL.Image.open("content.png"))
-style_image = np.array(PIL.Image.open("style.jpg"))
+content_image = np.array(PIL.Image.open("_content.png"))
+style_image = np.array(PIL.Image.open("_style.jpg"))
 
 # Define the image transformations
-# Preprocesses an input image in the format expected by the VGG-19 model
 preprocess = tf.keras.applications.vgg19.preprocess_input
 
+# Reshape the input images to have the expected shape
+content_image = tf.cast(tf.reshape(content_image, [1, *content_image.shape]),tf.float32)
+style_image = tf.cast(tf.reshape(style_image, [1, *style_image.shape]),tf.float32)
+
 # Apply the transformations to the images
-content_tensor = preprocess(content_image).astype('float32')
-style_tensor = preprocess(style_image).astype('float32')
+content_tensor = preprocess(content_image)#.astype('float32')
+style_tensor = preprocess(style_image)#.astype('float32')
 
 # Define the input image as a copy of the content image
 input_tensor = tf.Variable(content_tensor)